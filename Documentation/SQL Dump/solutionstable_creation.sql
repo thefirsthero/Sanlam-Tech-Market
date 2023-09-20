345019745ef166be56eeff6788fa7c40063829bb
DesireeMoeletsi <77278297+DesireeMoeletsi@users.noreply.github.com>
2023-09-20 08:40:55 +0200

    Created tables for the solutions

---
+++ Documentation/SQL Dump/solutionstable_creation.sql
@@ -0,0 +1,37 @@
+use policy_management_system;
+
+DROP TABLE IF EXISTS `solutions`;
+create table `solutions` (
+	solutionId int NOT NULL auto_increment primary key,
+    solution_name varchar(100),
+    solution_description varchar(250),
+    solution_path varchar(100),
+    solution_category varchar(100),
+    solution_tags varchar(100),
+    solution_snippet varchar(10000),
+    solution_link varchar(100)
+);
+
+/* Example add to table*/;
+
+insert into solutions(solution_name, solution_description, solution_path, solution_category, solution_tags, solution_snippet, solution_link) values (
+	"UiPath Bot", 
+    "Bot that auto-responds to basic user queries sent to the MiWay email box", 
+    "C://",
+    "Automation",
+    "Bots",
+    "import '../styles/App.css'; 
+    function Solution(props) {
+		return(
+			<div class=&quot;card&quot; >
+				<img src=&quot;...&quot; alt=&quot;...&quot;/>
+				<div class=&quot;card-body&quot;>
+					<h5 class=&quot;card-title&quot;>{ props.title }</h5>
+					<p class=&quot;card-text&quot;>{ props.text }</p>
+					<a href=&quot;#&quot; class=&quot;btn btn-primary&quot;>See Solution</a>
+				</div>
+			</div>
+		);
+	}
+	export default Solution;",
+    "");
